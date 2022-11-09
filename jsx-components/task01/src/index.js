7e385de33a20d7b868df6bbba8d61c98697eb214
Jeanne d'Arc NYIRAMWIZA <njoanc@gmail.com>
2022-11-09 12:22:42 +0200

    change():props name value

--- jsx-components/task01/src/index.js
+++ jsx-components/task01/src/index.js
@@ -1,28 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Profile } from "./Profile";
 
-function Profile(name) {
-  const personInfo = {
-    name: "Jeanne",
-  };
-  const element = (
-    <main id="content" role="main" className="base">
-      <h1>{name}'s React Page</h1>
-      <p>A woman in men's world</p>
-    </main>
-  );
-  if (name.toLowerCase() === personInfo.name.toLowerCase()) {
-    return element;
-  }
-  return (
-    <main id="content" role="main" className="base">
-      <h1>Just a React Page</h1>
-      <p>Nothing to see here!</p>
-    </main>
-  );
-}
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(Profile("Jeanne"));
-
-ReactDOM.createRoot(document.getElementById("root")).render(Profile("Jeanne"));
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Profile name="Liz" bio="My bio" />
+);
