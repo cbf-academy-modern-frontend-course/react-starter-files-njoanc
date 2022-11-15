ddecd1bd9943ed3251542fd657b0e19053c454fc
Jeanne d'Arc NYIRAMWIZA <njoanc@gmail.com>
2022-11-15 14:09:47 +0200

    update():use of multiple inputs approach

--- routes-and-forms/src/components/PetForm.js
+++ routes-and-forms/src/components/PetForm.js
@@ -1,30 +1,74 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const PetForm = () => {
-  const [name, setName] = useState('');
-  const [,setSubmitted] = useState(false);
+  const [inputs, setInputs] = useState({});
+  const [hasSubmitted, setSubmitted] = useState(false);
+
+  function handleChange(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
+  }
 
   function handleSubmit(event) {
     event.preventDefault();
     setSubmitted(true);
   }
-
   return (
     <div>
       <h2>Register Your Pet</h2>
       <h3>Please fill in your pet's details below</h3>
-      <form onSubmit={(e) => handleSubmit(e)}>
+      <form onSubmit={handleSubmit}>
         <div>
-          <label>Pet's Name:<input type="text" value={name} onChange={(e) => { setName(e.target.value); setSubmitted(false) }} /></label>
+          <label>
+            Pet's Name:
+            <input
+              type="text"
+              name="name"
+              value={inputs.name}
+              onChange={handleChange}
+            />
+          </label>
+          <br />
+          <br />
+          <label>
+            Pet's Age:
+            <input
+              type="text"
+              name="age"
+              value={inputs.age}
+              onChange={handleChange}
+            />
+          </label>
+          <br />
+          <br />
+
+          <label>
+            Pet's Colors:
+            <input
+              type="text"
+              name="colors"
+              value={inputs.colors}
+              onChange={handleChange}
+            />
+          </label>
+          <br />
+          <br />
         </div>
-        <input type="submit" value="Submit" id="submit-button"/>
+        <input type="submit" value="Submit" id="submit-button" />
       </form>
+      {hasSubmitted === true && <p>Thank you for submitting</p>}
       <div id="results">
         <h3>Results</h3>
-            Name: {name}<br />
+        Name: {inputs.name}
+        <br />
+        Age: {inputs.age}
+        <br />
+        Colors:{inputs.colors}
+        <br />
       </div>
     </div>
   );
-}
+};
 
 export default PetForm;
