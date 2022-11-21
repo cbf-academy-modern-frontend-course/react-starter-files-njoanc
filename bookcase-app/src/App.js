ac01f402400093080286e7460ac6b382378c9d95
Jeanne d'Arc NYIRAMWIZA <njoanc@gmail.com>
2022-11-21 12:52:06 +0200

    add():style and routes

--- bookcase-app/src/App.js
+++ bookcase-app/src/App.js
@@ -1,17 +1,40 @@
 import React, { useState } from "react";
-import "./index.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Book from "./components/Book";
 import data from "./models/books.json";
 import BookList from "./components/BookList";
+import AboutUsPage from "./pages/About";
+import Home from "./components/Home";
 
-const App = () => {
+function App() {
   const [books] = useState(data);
-
+  function addBook(title) {
+    console.log(`The Book ‘${title}’ was clicked`);
+  }
   return (
-    <div className="container">
-      <h1 className="title">Bookcase App</h1>
-      <BookList books={books} />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Header />}>
+          <Route index element={<Home />} />
+          <Route path="/about" element={<AboutUsPage />} />
+          <Route
+            path="/bookcase"
+            element={
+              <BookList>
+                {books.map((book) => (
+                  <Book
+                    key={book.id}
+                    book={book}
+                    addBook={() => addBook(book.volumeInfo.title)}
+                  />
+                ))}
+              </BookList>
+            }
+          />
+        </Route>
+      </Routes>
+    </Router>
   );
-};
-
+}
 export default App;
--- bookcase-app/src/components/Button.js
+++
@@ -1,9 +0,0 @@
-const Button = ({ handleClick, text }) => {
-  return (
-    <button className="button" onClick={handleClick}>
-      {text}
-    </button>
-  );
-};
-
-export default Button;
