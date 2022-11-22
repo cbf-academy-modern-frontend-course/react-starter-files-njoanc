028e5dcf08af11b4e98e52999b1f25402617c7fd
Jeanne d'Arc NYIRAMWIZA <njoanc@gmail.com>
2022-11-22 09:03:35 +0200

    add():routes and styling

--- bookcase-app/src/components/BookList.js
+++ bookcase-app/src/components/BookList.js
@@ -1,10 +1,8 @@
 import React from "react";
-import Search from "./Search";
 import "../index.css";
 const BookList = (props) => {
   return (
     <div>
-      <Search />
       <div className="container">{props.children}</div>;
     </div>
   );
